efc6f261eac25981783e7fb63624c0097bfb0bed
Bala Yogesh <balayogesh2000@gmail.com>
2020-12-02 14:43:26 +0530

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    siteMetadata: {
+        title: 'Full-Stack Bootcamp',
+        author: 'Andrew Mead'
+    },
+    plugins: [
+        'gatsby-plugin-sass',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'src',
+                path: `${__dirname}/src/`
+            }
+        }
+    ]
+}
